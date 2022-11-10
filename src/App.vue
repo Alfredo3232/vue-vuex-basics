a41f8a371d552f13db5705d9c4013a65004a0d9a
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-10 17:37:53 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <button @click="addOne">Add 1</button>
+    <change-counter></change-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue'
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter
+  },
+  methods: {
+    addOne() {
+      this.$store.commit('increase', { value: 10 });
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>

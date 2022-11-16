6248097bd8c6cf947e3d8067f5ae98f739805c32
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-15 19:37:08 -0600

    implementing actions

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ export default {
     addOne() {
       //.this.$store.commit('increase', { value: 10 });
 
-      this.$store.commit({
+      this.$store.dispatch({
         type: 'increase',
         value: 10
       });

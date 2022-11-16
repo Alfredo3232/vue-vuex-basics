df41b2583a4a43cb259ca12e06a5528eacc4c04e
arojas32 <61888719+arojas32@users.noreply.github.com>
2022-11-15 20:34:02 -0600

    add auth component logic & seperated counter logic

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-const store = createStore({
+const counterModule = {
     state() {
         return {
             counter: 0
@@ -40,6 +40,35 @@ const store = createStore({
                 return 100;
             }
             return finalCounter;
+        }
+    }
+}
+
+const store = createStore({
+    modules: {
+        numbers: counterModule
+    },
+    state() {
+        return {
+            isLoggedIn: false
+        };
+    },
+    mutations: {
+        setAuth(state, payload) {
+            state.isLoggedIn = payload.isAuth;
+        }
+    },
+    actions: {
+        login(context) {
+            context.commit('setAuth', { isAuth: true });
+        },
+        logout(context) {
+            context.commit('setAuth', { isAuth: false });
+        }
+    },
+    getters: {
+        userIsAuthenticated(state) {
+            return state.isLoggedIn;
         }
     }
 });
